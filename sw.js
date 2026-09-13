232641730841372289e7e7b13adaa42a116fe1eb
dowakin1991-create <dowakin1991@gmail.com>
2026-09-13 17:52:26 +0300

    Update cache version to achilles-os-v10-5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'achilles-os-v10-4';
+const CACHE_VERSION = 'achilles-os-v10-5';
 const APP_SHELL = ['./', './index.html', './IMG_9302.jpeg', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
