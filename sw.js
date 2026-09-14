442b412b894f2babcb988aed792d59708753c30c
dowakin1991-create <dowakin1991@gmail.com>
2026-09-14 17:28:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'achilles-os-v10-8';
+const CACHE_VERSION = 'achilles-os-v10-9';
 const APP_SHELL = ['./', './index.html', './IMG_9302.jpeg', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
