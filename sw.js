77a650713a34bfb33864289f7a1386c198051bc5
dowakin1991-create <dowakin1991@gmail.com>
2026-09-20 00:39:08 +0300

    Precache avatar progression assets in 11.2.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'achilles-os-v11-1-1';
+const CACHE_VERSION = 'achilles-os-v11-2-0';
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -25,6 +25,8 @@ const APP_SHELL = [
   "./core-data.js",
   "./training.js",
   "./app-runtime.js",
+  "./avatar-system.js",
+  "./assets/avatars/achilles-avatar-atlas.webp",
   "./system-ui.js",
   "./nutrition-reliability.js",
   "./pwa.js",
