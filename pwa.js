c283bda72a49e62c82ad0761bb0ef8df8c06cbc8
dowakin1991-create <dowakin1991@gmail.com>
2026-09-19 13:28:08 +0300

    Register service worker 10.18.3

--- pwa.js
+++ pwa.js
@@ -4,7 +4,7 @@
     if (!('serviceWorker' in navigator)) return;
     window.addEventListener('load', async () => {
         try {
-            const reg = await navigator.serviceWorker.register('./sw.js?v=10.18.2', { scope: './', updateViaCache: 'none' });
+            const reg = await navigator.serviceWorker.register('./sw.js?v=10.18.3', { scope: './', updateViaCache: 'none' });
             reg.addEventListener('updatefound', () => {
                 const worker = reg.installing;
                 if (!worker) return;
